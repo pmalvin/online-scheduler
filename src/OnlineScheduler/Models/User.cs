f51f4d79eef47717398140c689538fa2d11a545c
pmalvin <alvingao97@outlook.com>
2017-09-20 20:34:46 +0800

    User的模型

--- src/OnlineScheduler/Models/User.cs
+++ src/OnlineScheduler/Models/User.cs
@@ -1,11 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace OnlineScheduler.Models
 {
     public class User
     {
-        public int UserId { get; set;}
-        public string UserName { get; set;}
-        public DateTimeOffset RegisterDate { get; set;}
-        public string EmailAddress { get; set;}
-        public string PassWord { get; set;}
+        [Key]
+        public string EmailAddress { get; set; }
+
+        [MaxLength(30)]
+        public string UserName { get; set; }
+        public DateTimeOffset RegisterDate { get; set; }
+
+        [MaxLength(30), MinLength(10)]
+        public string PassWord { get; set; }
     }
 }
