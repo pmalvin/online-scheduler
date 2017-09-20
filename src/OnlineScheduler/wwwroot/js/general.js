45896824bbfe1259ac586de2b26901a07fff0290
pmalvin <alvingao97@outlook.com>
2017-09-20 10:45:53 +0800

    写一个简单的欢迎页面

--- src/OnlineScheduler/wwwroot/js/general.js
+++ src/OnlineScheduler/wwwroot/js/general.js
@@ -1,3 +1,27 @@
 /*这是该项目的默认js文件
  *各个页面也会有自己的js文件
  */
+var sideBarVm = new Vue({
+    el: '#side-bar',
+    data: {
+        page: currentPage //定义在了layout的head部分
+    },
+    computed: {
+        selectedItem: function () {
+            return this.page;
+        }
+    }
+});
+
+var headerVm = new Vue({
+    el: '#header',
+    data: {
+        isLoggedIn: false,
+        userName: ''
+    },
+    methods: {
+        toggleLoggedIn: function () {
+
+        }
+    }
+});
